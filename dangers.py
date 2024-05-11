066da2a0cc2e11e7fa25d4ea499b2926a408c18c
David <defaultik.personal@gmail.com>
2024-05-11 16:57:09 +0300

    improved code readable

--- dangers.py
+++ dangers.py
@@ -4,30 +4,32 @@ import constants as Constants
 
 
 class Danger:
-    def __init__(self, obj, player):
+    def __init__(self, obj):
         self.obj = obj
-        self.player = player
+        self.player = self.obj.player
 
 
 class Rocket(Danger):
-    def __init__(self, obj, player):
-        super().__init__(obj, player)
+    def __init__(self, obj):
+        super().__init__(obj)
 
+        self.warning = True
         self.warning_counter = 0
-
-        self.spawn_time = 0
-        self.spawn_interval = random.randint(60, 300)
 
         self.sprite = pygame.sprite.Sprite()
         self.sprite.image = Constants.ROCKET_IMAGE.convert_alpha()
+
         self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect.x = Constants.WIDTH - 60
+        self.sprite.rect.y =  Constants.HEIGHT / 2
+
         self.sprite.mask = pygame.mask.from_surface(self.sprite.image)
 
 
     def draw(self):
         for rocket in self.obj.rockets:
-            if rocket["warning"]:
-                self.draw_warning(rocket)
+            if self.warning:
+                self.draw_warning()
             else:
                 self.draw_rocket(rocket)
 
@@ -36,45 +38,39 @@ class Rocket(Danger):
                     self.sprite.image = Constants.ROCKET_EXPLOSION_IMAGE
                     self.sprite.rect = self.player.sprite.rect
 
-                if self.sprite.rect.x <= 0:
-                    self.obj.rockets.remove(rocket)
-                    del rocket
 
-
-    def draw_warning(self, rocket):
-        rocket_warning = pygame.draw.rect(self.obj.screen, "crimson", [rocket["x"], rocket["y"], 50, 50], 0, 5)
+    def draw_warning(self):
+        rocket_warning = pygame.draw.rect(self.obj.screen, "crimson", [self.sprite.rect.x, self.sprite.rect.y, 50, 50], 0, 5)
         text_surface = Constants.FONT_ROCKET_WARNING.render("!", True, "white")
         text_rect = text_surface.get_rect(center=rocket_warning.center)
         self.obj.screen.blit(text_surface, text_rect)
 
-        if (not self.obj.pause_menu.active) and (not self.obj.question_menu.active):
-            if rocket["y"] > self.player.sprite.rect.y:
-                rocket["y"] -= 6
+        if (not self.obj.pause_menu.active) and (not self.obj.game_over_menu.active) and (not self.obj.main_menu.active) and (not self.obj.question_menu.active):
+            if self.sprite.rect.y > self.player.sprite.rect.y:
+                self.sprite.rect.y -= 6
             else:
-                rocket["y"] += 6
+                self.sprite.rect.y += 6
 
             self.warning_counter += 1
             if self.warning_counter >= 45:
                 self.warning_counter = 0
-                rocket["warning"] = False
+                self.warning = False
 
 
     def draw_rocket(self, rocket):
-        self.sprite.rect.x = rocket["x"]
-        self.sprite.rect.y = rocket["y"]
-
         self.obj.screen.blit(self.sprite.image, self.sprite.rect)
 
         if (not self.obj.pause_menu.active) and (not self.obj.game_over_menu.active) and (not self.obj.main_menu.active) and (not self.obj.question_menu.active):
-            rocket["x"] -= 15 + self.obj.game_speed
+            self.sprite.rect.x -= 15 + self.obj.game_speed
+
+        if self.sprite.rect.x <= (0 - self.sprite.mask.get_size()[0]):
+            self.obj.rockets.remove(rocket)
+            del rocket
 
 
 class Laser(Danger):
-    def __init__(self, obj, player):
-        super().__init__(obj, player)
-
-        self.spawn_time = 0
-        self.spawn_interval = random.randint(60, 300)
+    def __init__(self, obj):
+        super().__init__(obj)
 
 
     def draw(self):
